6fae6aa7f246c44785a77efaf2952544d4b8151f
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2025-02-02 08:38:48 -0500

    feat: added Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientProvider from "../provider/clientProvider"; 
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <ClientProvider>{children}</ClientProvider>
+        <Analytics />
       </body>
     </html>
   );
